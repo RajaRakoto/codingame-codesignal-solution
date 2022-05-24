ef8659ecd4ad896cc5305e01fb23e0cb4b8d0067
raja <raja.rakoto7@gmail.com>
2022-05-24 10:37:01 +0300

    add

---
+++ array-counter.js
@@ -0,0 +1,20 @@
+/*
+ Votre programme doit imprimer un array en fonction de deux valeurs en entree dont L represente la longueur de chaque tableau et N represente le nombre de tableau a afficher, l'element du tableau est un valeur incrementE allant de 1 | ex: L = 2, N = 4 -> [ 1, 2 ] [ 3, 4 ] [ 5, 6 ] [ 7, 8 ]
+*/
+
+const L = 2; // length of array
+const N = 4; // number of array
+
+let arrayIndiv = [];
+let arrayGlob = [];
+let elem = 1;
+
+for (let i = 0; i < N; i++) {
+	for (let j = 0; j < L; j++) {
+		arrayIndiv.push(elem++);
+	}
+	arrayGlob.push(arrayIndiv);
+	arrayIndiv = []; // vider le tableau
+}
+
+console.log(...arrayGlob);
