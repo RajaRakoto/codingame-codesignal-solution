1ae76ed95ec8ff8f48fed031f9bf0e0a271f64b8
raja <raja.rakoto7@gmail.com>
2022-05-25 13:13:23 +0300

    add

---
+++ binary-not.js
@@ -0,0 +1,14 @@
+/* Votre programme doit imprimer le NOT (inverse) d'un nombre binaire B | ex: B = 101100 -> 010011 */
+
+// Methode 1
+const B = '101100';
+let result = [];
+
+Array.from(B).forEach(b => {
+	b === '1' ? result.push('0') : result.push('1');
+});
+
+console.log(Number(result.join('')));
+
+// Methode 2
+console.log(Number(B.replaceAll('1', 'a').replaceAll('0', '1').replaceAll('a', '0')));
