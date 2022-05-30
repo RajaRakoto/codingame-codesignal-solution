7154dd95327bea081897225b188f289a2eff7aa2
raja <raja.rakoto7@gmail.com>
2022-05-30 09:22:03 +0300

    to func

--- alphabet-sum.js
+++ alphabet-sum.js
@@ -3,11 +3,15 @@
  * @exemple n = "HELLO WORLD !" | result -> 114 | n = "AAAA =-" | result -> 0
  * @feat - Array.from() | map() | indexOf()
  */
-const n = "HELLO WORLD !" ;
-const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let sum = 0;
-Array.from(n).map(n => {
-	sum +=
+function alphabetSum() {
+  
+  const n = "HELLO WORLD !" ;
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let sum = 0;
+  Array.from(n).map(n => {
+    sum +=
 		alphabet.indexOf(n) === -1 ? alphabet.indexOf(n) + 1 : alphabet.indexOf(n);
 });
 console.log(sum);
+}
+alphabetSum()
