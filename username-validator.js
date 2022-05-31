bfc769c4968b39dc597377c4b0329788912fb3c8
raja <raja.rakoto7@gmail.com>
2022-05-31 09:36:23 +0300

    add

---
+++ username-validator.js
@@ -0,0 +1,17 @@
+/**
+ * @sujet - Vous devez ecrire une programme qui verifie si le nom d'utilisateur donnE "u" est valable | un nom d'utilisateur doit: avoir au moins 3 caracteres, ne pas depasser 20 caracteres, ne comporter que des lettres et des chiffres [a-z] [A-Z] [0-9] | si le nom d'utilisateur repond a ces exigences, imprimez "VALID" sinon "INVALID"
+ * @exemple -  u = "rakotoNirina07" | result -> VALID | u = "_rasoa5_" | result -> INVALID
+ * @feat - test()
+ */
+function userNameValidator() {
+	const u = 'rakotoNirina07';
+	const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+	if (u.length >= 3 && u.length <= 20 && !format.test(u)) {
+		console.log('VALID');
+	} else {
+		console.log('INVALID');
+	}
+}
+
+userNameValidator();
