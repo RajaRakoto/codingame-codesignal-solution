53105f5212c05c4332de550fcf666cc2250cc229
raja <raja.rakoto7@gmail.com>
2022-06-15 12:27:55 +0300

    add

---
+++ charcode-manip.js
@@ -0,0 +1,19 @@
+/**
+ * @sujet - Etant donnE une chaine d'entree "s", renvoie le caractere donne par la moyenne des valeurs ASCII de tous les caracteres de la chaine. "s" peut contenir des caracteres ASCII, des lettres majuscules, des lettres miniscules, des espaces ou des symboles. Si la valeur moyenne est un flottant, arrondissez vers le bas
+ * @exemple - s = "hello" | result -> "j" | s = "HelLO wOrlD" | result -> "V" | s = "H3LL0 W0RLD" | result -> "A"
+ * @feat - fromCharCode() | Math.floor() | Array.from() | map() | reduce()
+ */
+function charcodeManip() {
+	const s = 'HelLO wOrlD';
+
+	console.log(
+		String.fromCharCode(
+			Math.floor(
+				[...s].map(s => s.charCodeAt()).reduce((total, c) => total + c) /
+					s.length,
+			),
+		),
+	);
+}
+
+charcodeManip();
