c9418c038d63d70acefdd2f4b86640a00871c471
raja <raja.rakoto7@gmail.com>
2022-06-21 17:30:44 +0300

    add

---
+++ vowels-counter.js
@@ -0,0 +1,11 @@
+/**
+ * @sujet - Imprimez le total des voyelles dans une chaine donnEe
+ * @exemple - s = "Hello World" | result -> 3
+ * @feat - match()
+ */
+function vowelsCounter() {
+	s = 'Hello World';
+	console.log(s.match(/[aeiou]/gi).length);
+}
+
+vowelsCounter();
