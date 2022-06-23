56f9e395902651b6418baa869cb7d099f2bda02b
raja <raja.rakoto7@gmail.com>
2022-06-23 11:07:43 +0300

    add

--- alphabet-index-sum.js
+++ alphabet-index-sum.js
@@ -1,7 +1,6 @@
 /**
  * @sujet - Les lettres de l'alphabet occupent les positions A=1, B=2, C=3... Lorsqu'elles sont classEs par ordre alphabetique. Calculer la somme des positions alphabetiques des lettres dans la phrase donnEe. Les positions ne sont pas sensibles a la casse et les non-lettres sont ignorEes
  * @exemple - str = "a" | result -> 1 | str = "ab!" | result -> 3 | str = "Hello" | result -> 52
- * @feat -
  */
 function alphabetIndexSum() {
 	const str = 'Hello';
