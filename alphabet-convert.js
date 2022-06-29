bf9b8c8e52d5d81cf0ac246b92d2e6c2ee53ad19
raja <raja.rakoto7@gmail.com>
2022-06-29 12:55:08 +0300

    add

---
+++ alphabet-convert.js
@@ -0,0 +1,16 @@
+/**
+ * @sujet - Votre programme doit convertir tout les caracteres de l'alphabet a l'index correspondant
+ * @exemple str = "gdi" | result -> 749 | str = "ajul" | result -> 1102112 | str = "21g3abt" | result -> 21791220
+ * @feat - map() | toLowerCase() | fromCharCode() | keys() | slice() | indexOf() | join()
+ * @note - Les caracteres speciaux et numeriques restent le meme
+ */
+function alphabetConvert() {
+	const str = '21g3abt';
+	const str2 = [...str].map(s => s.toLowerCase());
+	const alphabet = [...String.fromCharCode(...Array(123).keys()).slice(97)];
+	const result = str2.map(s =>
+		alphabet.indexOf(s) == -1 ? s : alphabet.indexOf(s) + 1,
+	);
+	console.log(result.join``);
+}
+alphabetConvert();
