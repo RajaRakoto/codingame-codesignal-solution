2e88ad6d6193d26f2e95109bbe77cf504b88ee4c
raja <raja.rakoto7@gmail.com>
2022-07-27 10:14:28 +0300

    add

---
+++ revert-case.js
@@ -0,0 +1,19 @@
+/**
+ * @sujet - Votre programme doit inverser la casse d'une chaîne donnée. difference entre ce nombre est son inverse
+ * @exemple - s = "HellO wOrlD !" | result -> "hELLo WoRLd !"
+ * @feat - test() | toLowerCase() | toUpperCase() | join()
+ */
+
+function revertCase() {
+	const s = 'HellO wOrlD !';
+
+	const isUpper = str => {
+		return !/[a-z]/.test(str) && /[A-Z]/.test(str);
+	};
+
+	console.log(
+		[...s].map(s => (isUpper(s) ? s.toLowerCase() : s.toUpperCase())).join``,
+	);
+}
+
+revertCase();
