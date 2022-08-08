324d094284b741e7c69d223404a328728b46c9aa
raja <raja.rakoto7@gmail.com>
2022-08-08 09:08:41 +0300

    add

---
+++ sentence-reverse.js
@@ -0,0 +1,35 @@
+/**
+ * @sujet - Ecrire l'algorithme correspondant au exemple ci-dessous
+ * @exemple 
+ * m = "Chuck Norris"
+ * output:
+  C s
+  h i
+  u r
+  c r
+  k o
+    N
+  N 
+  o k
+  r c
+  r u
+  i h
+  s C
+ * @feat - split() | forEach() | reverse()
+ */
+function sentenceReverse() {
+  const m = "Chuck Norris".split('');
+  let n = [];
+  
+  m.forEach(m => {
+    n.push(m);
+  })
+  
+  n = n.reverse();
+  
+  m.forEach((m, i) => {
+    console.log (m + " " + n[i]);
+  })
+}
+  
+sentenceReverse();
